317418cc085f2785721c39e1a6bc14274c4f3ca2
MrManoj124 <manojmanorooban@gmail.com>
2025-12-02 23:48:47 +0530

    Delete the unwated files in models

--- backend/models/course.js
+++
@@ -1,9 +0,0 @@
-const courseSchema = new mongoose.Schema({
-    code:{type:String, required:true, unique:true},
-    name:{type:String, required:true },
-    credits:{type:Number, required:true},
-    semester:{type:String, required:true},
-    department:String,
-    description:String,
-    createdAt:{type:Date, default:Date.now}
-});
