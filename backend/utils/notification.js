205eeaf50648f032be7cab97d69d46dd69c77565
MrManoj124 <manojmanorooban@gmail.com>
2025-12-08 23:59:23 +0530

    Code for utils/notification.js file

--- backend/utils/notification.js
+++ backend/utils/notification.js
@@ -1,3 +1,53 @@
-const Notification = require('../models/Notification'); 
-//const {sendEmail}= require('./emailService');
+const Notification = require('../models/Notification');
+const { sendEmail } = require('./email');
 
+exports.sendNotification = async (notificationData) => {
+  try {
+    // Create notification in database
+    const notification = await Notification.create(notificationData);
+
+    // Send email if enabled
+    if (notificationData.channels && notificationData.channels.email) {
+      await sendEmail({
+        to: notificationData.recipientEmail,
+        subject: notificationData.title,
+        template: 'notification',
+        data: {
+          title: notificationData.title,
+          message: notificationData.message,
+          actionUrl: notificationData.actionUrl
+        }
+      });
+    }
+
+    // Send push notification if enabled
+    if (notificationData.channels && notificationData.channels.push) {
+      // Implement Firebase Cloud Messaging here
+      await sendPushNotification(notification);
+    }
+
+    return notification;
+  } catch (error) {
+    console.error('Error sending notification:', error);
+    throw error;
+  }
+};
+
+exports.sendBulkNotifications = async (recipients, notificationData) => {
+  const notifications = recipients.map(recipient => ({
+    ...notificationData,
+    recipient: recipient._id,
+    recipientEmail: recipient.email
+  }));
+
+  return await Promise.allSettled(
+    notifications.map(n => this.sendNotification(n))
+  );
+};
+
+// Firebase push notification helper
+const sendPushNotification = async (notification) => {
+  // Implement Firebase Admin SDK push notification
+  // This is a placeholder - implement based on your Firebase setup
+  console.log('Push notification sent:', notification.title);
+};
