93485e7dc2a62730bcbdf72cc48439fc912714bd
MrManoj124 <manojmanorooban@gmail.com>
2025-12-10 00:18:51 +0530

    Update studentRoute.js file

--- backend/routes/Studentroute.js
+++ backend/routes/Studentroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const studentController = require('../controllers/student.controller');
+const studentController = require('../controllers/user');
 const { authenticate, authorize } = require('../middleware/auth');
 
 router.use(authenticate);
