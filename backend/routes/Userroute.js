417900e5114cf8167e05077e879bd375d3e1a3a3
MrManoj124 <manojmanorooban@gmail.com>
2025-12-12 10:42:44 +0530

    modify the controllers/user.js file

--- backend/routes/Userroute.js
+++ backend/routes/Userroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/user');
 const { authenticate, authorize } = require('../middleware/auth');
 
 // All routes require authentication and admin role
