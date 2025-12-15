66ae69bf597ab866662390376a20dc3ecc1de8ba
MrManoj124 <manojmanorooban@gmail.com>
2025-12-15 21:11:20 +0530

    Define get and set method to the StaffDashborad.js file

--- frontend/src/components/StaffDashboard.js
+++ frontend/src/components/StaffDashboard.js
@@ -4,3 +4,14 @@ import { GraduationCap, Logout, BookOpen, Users, FileText,
     }  from 'lucide-react';
 import { staffAPI, resultAPI, courseAPI, studentAPI, calculateGrade, handleAPIError } from '../services/api';
 
+const StaffDashboard = ({user, onLogout})=>{
+    const [activeTab, setActiveTab] = useState('dashboard');
+    const [stats, setStats] = useState(null);
+    const [courses, setCourses] = useState([]);
+    const [students, setStudents] = useState([]);
+    const [result, setResults] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [showAddResultModal, setShoqAddResultModal] = useState(false);
+
+    
+}
